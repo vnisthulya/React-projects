fd513411ea764255871809de7f045cc45e082d05
Vinod S <vinodnisthulya@gmail.com>
2025-02-10 21:47:01 +0530

    ignite app

--- App.js
+++ App.js
@@ -1,26 +1,16 @@
 //
-//
-//<div id=parent>
-    //<div id="c1">
-        //<h1 id="heading">Hello World! Im React.</h1>
-        //<p id="para"> getting ready to become a - conqueror! </p>
-        //<p id="para">That's a fantastic attitude!  "Conqueror" implies overcoming challenges and achieving victory. Here are some ways to express that you're getting ready to become a conqueror, depending on the context: </p>
-    //</div>
-    //<div id="c2">
-        //<h1 id="heading"> Mission </h1>
-        //<p id="para"> Get ready for 40 </p>
-        //<p id="para"> Turning 40 is a big milestone! It's a time for reflection, celebration, and looking forward to the exciting chapters ahead. Here are some ways to express "Get ready for 40," </p>
-    //</div>
-//</div>
-//
-//
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+
 
 const parent= React.createElement("div", {id: "parent"}, 
     [React.createElement("div", {id: "c1"},
     [
         React.createElement("h1", {id:"heading"}, "Hello World! Im React."),
         React.createElement("p", {id:"para"}, "getting ready to become a - conqueror!"),
-        React.createElement("p", {id:"para"}, "That's a fantastic attitude!  \"Conqueror\" implies overcoming challenges and achieving victory. Here are some ways to express that you're getting ready to become a conqueror, depending on the context:")
+        React.createElement("p", {id:"para"}, "That's a fantastic attitude!  \"Conqueror\" implies overcoming challenges and achieving victory. Here are some ways to express that you're getting ready to become a conqueror, depending on the context: thats the beauty")
     ]),
     React.createElement("div", {id: "c2"},
     [
--- Readme.md
+++ Readme.md
@@ -1,1 +1,20 @@
 # React Learning Projects 
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - faster builds
+- image optimisation
+- minification
+- Bundling
+- compress proj files
+- consistent Hashing
+- code splitting 
+- Diffrencial bundling
+- Diagnostic
+- Error Handling
+- HTTPs 
+- Tree shaking - unused code
